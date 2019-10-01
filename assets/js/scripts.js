f4874a6db1f5a0b66136f63516c6e0e285c053ed
aqib21 <aqib.ahamad21@gmail.com>
2019-10-01 21:04:40 +0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,69 @@
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails"
+    });
+});
+
+jQuery(document).ready(function() {
+    $clientsHolder = $('ul.projects-img');
+    $clientsClone = $clientsHolder.clone(); 
+ 
+    $('.filter-projects a').click(function(e) {
+        e.preventDefault();
+        $filterClass = $(this).attr('class');
+ 
+        $('.filter-projects a').attr('id', '');
+        $(this).attr('id', 'active-imgs');
+ 
+        if($filterClass == 'all'){
+            $filters = $clientsClone.find('li');
+        }
+        else {
+            $filters = $clientsClone.find('li[data-type~='+ $filterClass +']');
+        }
+ 
+        $clientsHolder.quicksand($filters, {duration: 700}, function() {
+            $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false});
+        });
+    });
+});
+
+jQuery(document).ready(function() {
+    $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false});
+});
+
+jQuery(document).ready(function() {
+    $('.contact-form form').submit(function() {
+
+        $('.contact-form form .nameLabel').html('Name');
+        $('.contact-form form .emailLabel').html('Email');
+        $('.contact-form form .messageLabel').html('Message');
+
+        var postdata = $('.contact-form form').serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'assets/sendmail.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                if(json.nameMessage != '') {
+                    $('.contact-form form .nameLabel').append(' - <span class="blue" style="font-size: 13px; font-style: italic"> ' + json.nameMessage + '</span>');
+                }
+                if(json.emailMessage != '') {
+                    $('.contact-form form .emailLabel').append(' - <span class="blue" style="font-size: 13px; font-style: italic"> ' + json.emailMessage + '</span>');
+                }
+                if(json.messageMessage != '') {
+                    $('.contact-form form .messageLabel').append(' - <span class="blue" style="font-size: 13px; font-style: italic"> ' + json.messageMessage + '</span>');
+                }
+                if(json.nameMessage == '' && json.emailMessage == '' && json.messageMessage == '') {
+                    $('.contact-form form').fadeOut('fast', function() {
+                        $('.contact-form').append('<p><span class="blue">Thanks for contacting us!</span> We will get back to you very soon.</p>');
+                    });
+                }
+            }
+        });
+        return false;
+    });
+});
+
